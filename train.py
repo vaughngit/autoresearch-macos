ac983f99cc677dbdbf2a1226949ba3d2b0293bde
Alvin Vaughn <alvin.vaughn@outlook.com>
2026-05-21 21:53:25 -0500

    Revert "Experiment embedding LR 0.9"
    This reverts commit 919f235a6b49e228298d29ecee48ca0a432e9cef.

--- train.py
+++ train.py
@@ -486,7 +486,7 @@ WINDOW_PATTERN = "L"    # sliding window pattern: L=full, S=half context
 
 # Optimization
 TOTAL_BATCH_SIZE = 2**15 # ~33K tokens per optimizer step
-EMBEDDING_LR = 0.9      # learning rate for token embeddings (Adam)
+EMBEDDING_LR = 0.8      # learning rate for token embeddings (Adam)
 UNEMBEDDING_LR = 0.004  # learning rate for lm_head (Adam)
 MATRIX_LR = 0.03        # learning rate for matrix parameters (Muon)
 SCALAR_LR = 0.5         # learning rate for per-layer scalars (Adam)
