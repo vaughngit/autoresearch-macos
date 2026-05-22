1ad47930073bf2c3cab4db81ad44a0e4c2d77a5e
Alvin Vaughn <alvin.vaughn@outlook.com>
2026-05-21 19:43:06 -0500

    Experiment shallower model

--- train.py
+++ train.py
@@ -497,7 +497,7 @@ WARMDOWN_RATIO = 0.5    # fraction of time budget for LR warmdown
 FINAL_LR_FRAC = 0.0     # final LR as fraction of initial
 
 # Model size
-DEPTH = 4               # number of transformer layers
+DEPTH = 3               # number of transformer layers
 DEVICE_BATCH_SIZE = 16  # per-device batch size (reduce if OOM)
 
 # ---------------------------------------------------------------------------
