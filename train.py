68d644591a8aa32e668009d3af5e7bd795942426
Alvin Vaughn <alvin.vaughn@outlook.com>
2026-05-21 20:37:16 -0500

    Revert "Experiment extra wide two layer model"
    This reverts commit 33370c766c508203a56e8c3aadda2efe7642febc.

--- train.py
+++ train.py
@@ -480,7 +480,7 @@ class MuonAdamW(torch.optim.Optimizer):
 # ---------------------------------------------------------------------------
 
 # Model architecture
-ASPECT_RATIO = 192      # model_dim = depth * ASPECT_RATIO
+ASPECT_RATIO = 128      # model_dim = depth * ASPECT_RATIO
 HEAD_DIM = 128          # target head dimension for attention
 WINDOW_PATTERN = "L"    # sliding window pattern: L=full, S=half context
 
